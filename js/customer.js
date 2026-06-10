4c986af3cb90b61a84fd2b95a211e7ca480abd5b
しおわさび <mytr2dreams@gmail.com>
2026-06-10 18:34:03 +0900

    customer.js機能追加

---
+++ js/customer.js
@@ -0,0 +1,7 @@
+const customerForm = document.getElementById('customerForm');
+
+customerForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    // 入力値取得
+    console.log("登録されました");
+});
